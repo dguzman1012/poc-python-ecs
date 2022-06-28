4bd49ab1d9126b90f776d5ad59d51af5e1a18e6f
Daniel Guzman <dguzman1012@gmail.com>
2022-06-28 01:59:27 -0300

    final test

--- upload.py
+++ upload.py
@@ -1,21 +1,25 @@
 
 import os
+from threading import currentThread
 from dotenv import load_dotenv
 import boto3
+import time
 
 load_dotenv()  # take environment variables from .env
+
+currentTime = time.strftime('%a, %d %b %Y %H:%M:%S %Z(%z)')
 
 #Creating Session With Boto3.
 session = boto3.Session()
 #Creating S3 Resource From the Session.
 s3 = session.resource('s3')
-txt_data = b'This is the content of the file uploaded from python boto3!!!'
+txt_data = 'This is the content of the file uploaded from python boto3: ' + currentTime
 
 bucket = os.getenv('BUCKET', 'cor-local')
 
 print(bucket)
 
-object = s3.Object(bucket, 'test_python.txt')
+object = s3.Object(bucket, 'poc-python/test_python.txt')
 result = object.put(Body=txt_data)
 
 print("Done!")
